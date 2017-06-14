e3ca64a0ae230c30eefcf8cee63ca414fcc05025
Pavel Boldin <pboldin@cloudlinux.com>
2017-06-14 14:19:03 +0300

    update GHOST sample
    Change-Id: I41ee20e478e688e2c429577239d5b5747ec8f5a1
    Signed-off-by: Pavel Boldin <pboldin@cloudlinux.com>

--- samples/ghost/GHOST.c
+++ samples/ghost/GHOST.c
@@ -21,31 +21,31 @@ struct canary temp;
 
 void *thread(void *data)
 {
-  struct hostent resbuf;
-  struct hostent *result;
-  int herrno;
-  int retval;
+	struct hostent resbuf;
+	struct hostent *result;
+	int herrno;
+	int retval;
 
-  while (1) {
-    /*** strlen (name) = size_needed - sizeof (*host_addr) - sizeof (*h_addr_ptrs) - 1; ***/
-    size_t len = sizeof(temp.buffer) - 16*sizeof(unsigned char) - 2*sizeof(char *) - 1;
-    char name[sizeof(temp.buffer)];
-    memset(name, '0', len);
-    name[len] = '\0';
+	while (1) {
+		/*** strlen (name) = size_needed - sizeof (*host_addr) - sizeof (*h_addr_ptrs) - 1; ***/
+		size_t len = sizeof(temp.buffer) - 16*sizeof(unsigned char) - 2*sizeof(char *) - 1;
+		char name[sizeof(temp.buffer)];
+		memset(name, '0', len);
+		name[len] = '\0';
 
-    memcpy(&temp, &orig, sizeof(temp));
+		memcpy(&temp, &orig, sizeof(temp));
 
-    retval = gethostbyname_r(name, &resbuf, temp.buffer, sizeof(temp.buffer), &result, &herrno);
+		retval = gethostbyname_r(name, &resbuf, temp.buffer, sizeof(temp.buffer), &result, &herrno);
 
-    if (strcmp(temp.canary, CANARY) != 0) {
-      putc('.', stderr);
-    }
-    if (retval == ERANGE) {
-      //puts("not vulnerable");
-      return NULL;
-    }
-    sleep(1);
-  }
+		if (strcmp(temp.canary, CANARY) != 0) {
+			putc('.', stderr);
+		}
+		if (retval == ERANGE) {
+			//puts("not vulnerable");
+			return NULL;
+		}
+		sleep(1);
+	}
 }
 
 #define NTHREADS	16
